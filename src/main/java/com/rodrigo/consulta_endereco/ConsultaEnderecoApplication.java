afd3ccf7af3d5ad761f39160dbf4943c576a7c65
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-02-27 15:43:18 -0300

    Projeto consulta CEP API

---
+++ src/main/java/com/rodrigo/consulta_endereco/ConsultaEnderecoApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.consulta_endereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaEnderecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaEnderecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigo/consulta_endereco/ConsultaEnderecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.consulta_endereco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaEnderecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
