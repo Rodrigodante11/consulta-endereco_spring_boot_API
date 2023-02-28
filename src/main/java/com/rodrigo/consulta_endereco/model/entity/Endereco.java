6abc0fbe74dd054246bdfed0783df4771f96bbb7
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-02-27 23:44:08 -0300

    Adicionando banco em memoria h2 para os testes e os primeiros testes do repository

--- src/main/java/com/rodrigo/consulta_endereco/model/entity/Endereco.java
+++ src/main/java/com/rodrigo/consulta_endereco/model/entity/Endereco.java
@@ -1,27 +1,31 @@
 package com.rodrigo.consulta_endereco.model.entity;
 
+import com.rodrigo.consulta_endereco.model.enums.Regiao;
 import lombok.*;
-
 import javax.persistence.*;
+import java.util.Objects;
 
+
+//@Data
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
-@Entity
 @Table( name="endereco", schema = "consulta_endereco")
+@Entity
 public class Endereco {
-
     @Id
-    @Column(name="id")
-    private Long cep;
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // deixei para o banco dizer que eh auto incremento (bigserial postgre)
+    private Long id;
+
+    @Column(name="cep", unique = true)
+    private String cep;
 
     @Column(name="rua")
     private String rua;
     @Column(name="complemento")
     private String complemento;
-
     @Column(name="bairro")
     private String bairro;
     @Column(name="cidade")
@@ -29,5 +33,19 @@ public class Endereco {
     @Column(name="estado")
     private String estado;
     @Column(name="regiao")
-    private Double regiao;
+    @Enumerated(value =  EnumType.STRING )
+    private Regiao regiao;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Endereco endereco = (Endereco) o;
+        return Objects.equals(id, endereco.id) && cep.equals(endereco.cep) && Objects.equals(rua, endereco.rua) && Objects.equals(complemento, endereco.complemento) && Objects.equals(bairro, endereco.bairro) && cidade.equals(endereco.cidade) && estado.equals(endereco.estado) && regiao == endereco.regiao;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, cep, rua, complemento, bairro, cidade, estado, regiao);
+    }
 }
---
+++ src/main/java/com/rodrigo/consulta_endereco/model/enums/Regiao.java
@@ -0,0 +1,10 @@
+package com.rodrigo.consulta_endereco.model.enums;
+
+public enum Regiao {
+    SUDESTE,
+    CENTRO_OESTE,
+    NORDESTE,
+    SUL,
+    NORTE
+
+}
