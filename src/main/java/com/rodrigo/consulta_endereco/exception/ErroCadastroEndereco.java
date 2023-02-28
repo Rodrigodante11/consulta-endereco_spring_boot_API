cc125419a38ea99b9ba2665834dd8197b14c59df
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-02-28 10:48:00 -0300

    Adicionando novos testes do repository

---
+++ src/main/java/com/rodrigo/consulta_endereco/exception/ErroCadastroEndereco.java
@@ -0,0 +1,8 @@
+package com.rodrigo.consulta_endereco.exception;
+
+public class ErroCadastroEndereco extends RuntimeException{
+
+    public ErroCadastroEndereco(String msg){
+        super(msg);
+    }
+}
